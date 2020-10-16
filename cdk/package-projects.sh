87aadaac0b5ea5c931efc220914ddbe26801bf2f
Tiago Barbosa <tbarbos@hotmail.com>
2020-10-16 11:21:15 +0100

    added script that packages all projects

---
+++ cdk/package-projects.sh
@@ -0,0 +1,17 @@
+## Package AdClientAPI
+cd ../backend/src/AdClientAPI && npm install && sls package
+
+## Package AdPublisher
+cd ../AdPublisherAPI && npm install && sls package
+
+## Package AdReaderAPI
+cd ../AdReaderAPI && npm install && sls package
+
+## Package DynamoProcessor
+cd ../DynamoProcessor && npm install && sls package
+
+## Package ESProcessor
+cd ../ESProcessor && npm install && sls package
+
+## Return to CDK folder
+cd ../../../cdk
